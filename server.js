5e2599f3505e6041e2ad24d9d584498d79c3e509
rohit3345ks <rohit3345kumar@outlook.com>
2020-12-16 01:43:41 +0530

    Added Login/SignUp Views. Working on Authentication System

--- server.js
+++ server.js
@@ -1,12 +1,61 @@
 const express=require('express');
+const bodyParser=require('body-parser')
+const app=express();
+import mongoose from 'mongoose';
+const cors=require('cors');
 
-const app=express()
+var corsOptions={
+    origin: "http://localhost:3000"
+}
 
-app.use(express.static(__dirname+''))
+mongoose.connect()
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
+
+
+//Routes
+
+
+
+//************************ */
+
+//View Routes
+
+//****************************** */
 app.get('/',(req,res)=>{
-    res.send("Hello Node JS");
+    res.render('home');
 });
+
+// Login Page View Route
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+});
+
+// Login Post Route
+
+app.post('/login',(req,res)=>{
+    let usermail=req.body.usermail;
+    let pass=req.body.pass;
+    console.log("login");
+    console.log(usermail,pass);
+    res.render('home');
+});
+
+app.post('/signup',(req,res)=>{
+    let username=req.body.username;
+    let usermail=req.body.usermail;
+    let pass=req.body.pass;
+    console.log('Signup');
+    console.log(username,usermail,pass);
+    res.render('home');
+})
 
 
 app.listen(3000,()=>{
